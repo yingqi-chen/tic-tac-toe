7e18f3cef002e2e69e31036c5ed3d4760c4c9828
chanwkkk <chanwk18@hotmail.com>
2020-06-26 17:57:04 -0600

    add calculateWinner method and implement the handleClick method

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -31,10 +31,34 @@ export class BoardComponent implements OnInit {
     if(!this.squares[i]){
       this.squares.splice(i, 1, this.player)
       this.xIsNext  = !this.xIsNext
+      this.winner = this.calculateWinner()
     }else{
       alert("It has been taken!")
     }
+  }
 
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
   }
 
 }
