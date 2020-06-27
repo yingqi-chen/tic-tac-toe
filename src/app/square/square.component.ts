eaa3771da294d290d3f62412ac191450b080fdf6
chanwkkk <chanwk18@hotmail.com>
2020-06-27 15:19:59 -0600

    change square to button and install nebular

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
-  styleUrls: ['./square.component.scss']
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent implements OnInit {
 
   @Input() value: 'X' | 'O';
-  
+
   constructor() { }
 
   ngOnInit(): void {
