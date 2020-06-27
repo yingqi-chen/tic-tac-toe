2812cef6d93c8de1a55828045ea4a9ed12684779
chanwkkk <chanwk18@hotmail.com>
2020-06-27 15:28:23 -0600

    add button color to tic-tac-toe game

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'app-square',
   template: `
     <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="primary" *ngIf="value == 'X'">{{ value }}</button>
     <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
